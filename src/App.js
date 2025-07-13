eac3c2e6b331e74e3cddf3403daf590b4cb23e28
mddaniyaal <daniyalmohammad1642@gmail.com>
2025-07-13 18:23:44 +0530

    New project Hey Daniyal

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './gitlab.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://www.ferrari.com/en-US"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hey Daniyal
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
